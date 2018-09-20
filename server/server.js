e5fbcd53a674866b656a6be857ae7a515c12721f
fernando lopez <fernandolopez@MacBook-Pro.local>
2018-09-20 10:45:40 -0400

    Adding route for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const {ObjectId} = require('mongodb');
 const express = require( 'express' );
 const bodyParser = require( 'body-parser' );
 const {
@@ -50,10 +51,28 @@ app.post( '/todos', ( req, res ) => {
 app.get('/todos',(req, res) => {
         Todo.find()
             .then((todos) => {
-                return res.send(todos);
+                return res.send({todos});
             },(err) => {
                 console.log(err);
                 return res.status(400).send(err);
+             		}
+            )
+ 		});
+
+app.get('/todos/:id',(req, res) => {
+    if(!ObjectId.isValid(req.params.id)){
+        return res.status(404).send('Not a valid ID');
+    }
+
+        Todo.findById(req.params.id)
+            .then((todo) => {
+                if(!todo){
+                    return res.status(404).send('ID did not match any records');
+                }
+
+             res.status(200).send({todo});
+            },(err) => {
+                return res.status(400).send('Invalid ID');
              		}
             )
  		});
